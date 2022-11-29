94d2c007027999dfc77ac6c3e2f0744019effd88
AJ-Brown-InTech <ajalantbrown@gmail.com>
2022-11-28 20:30:41 -0600

    wss func

--- price.py
+++ price.py
@@ -1,3 +1,23 @@
-#view crypto currency market data
+#view market data in realtime
+import asyncio
+import websockets
+import os
+from dotenv import load_dotenv
 
-#websocket to get realtime data
+#load .env file and utilize vars
+load_dotenv()
+URL = os.getenv('BASE_URL')
+KEY = os.getenv('KEY')
+PORT = os.getenv('PORT')
+PHRASE = os.getenv('PHRASE')
+
+#wss func to retrieve coin tickers(prices)
+async def wss_func():
+   wss_connector = await websockets.connect(URL)
+   print(wss_connector.recv())
+   
+asyncio.get_event_loop().run_until_complete(wss_func())
+    
+    
+ 
+   
